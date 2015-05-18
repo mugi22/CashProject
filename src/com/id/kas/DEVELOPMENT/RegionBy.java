71e2a7ca6ece0bf1ae5ed644c1ae8bdeb8f8312d
mugiarto <mugihnf@gmail.com>
2015-05-18 16:24:09 +0700

    njn

---
+++ src/com/id/kas/DEVELOPMENT/RegionBy.java
@@ -0,0 +1,48 @@
+package com.id.kas.DEVELOPMENT;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.id.kas.util.AbstractListScreen;
+
+@Controller
+public class RegionBy extends AbstractListScreen{
+
+	
+
+	@RequestMapping(value="/regionBy.htm",method=RequestMethod.GET)
+	@Override
+	public String doGet(Map<String, Object> model, HttpSession session,	 HttpServletRequest reg, HttpServletResponse res ){
+		// TODO Auto-generated method stub
+		return super.doGet(model, session, reg,res);
+	}
+
+	@RequestMapping(value="/regionBy.htm",method=RequestMethod.POST)
+	@Override
+	public String doPost(Map<String, Object> model, HttpSession session, HttpServletRequest reg, HttpServletResponse res) {
+		// TODO Auto-generated method stub
+		System.out.println("DO POST");
+		return super.doPost(model, session,reg,res);
+	}
+	
+	
+//	 @RequestMapping(value="/testReport.htm", method=RequestMethod.GET)
+//     public String paramListAll(Map<String, Object> model,HttpSession session,HttpServletRequest reg) {
+//		 return "testReport";
+//	 }
+	@Override
+	protected String getView() {
+
+		return "util/regionBy";
+	}
+
+	
+	
+}
